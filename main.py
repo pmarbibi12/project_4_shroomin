69396231730be712f5a2bd1d8249d3ffe0abcd3f
pmarbibi12 <marbibipanfilo@gmail.com>
2023-10-07 20:08:29 -0500

    added predicting function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, jsonify, send_file
+from flask import Flask, render_template, jsonify, send_file, request
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
+from tensorflow import keras
+
+model = keras.models.load_model('shroom.h5')
 mushroom = fetch_ucirepo(id=73)
 X = mushroom.data.features 
 y = mushroom.data.targets
@@ -42,6 +45,48 @@ def display_targets():
 def download_model():
     model_path = 'shroom.h5'
     return send_file(model_path, as_attachment=True)
+
+@app.route('/api/v1/machine-learning/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the selected values from the POST request
+        selected_values = request.get_json()
+        print(selected_values)
+        print("Hello")
+
+        X_dummies = pd.get_dummies(X)
+        print(X_dummies.head())
+
+        # Preprocess the selected values (e.g., convert them to the format expected by your model)
+        user_input_df = pd.DataFrame(columns=X_dummies.columns)
+        # print(f"Empty DF: {user_input_df}")
+
+        for category, value in selected_values.items():
+            # Create a column name like "category_value" based on user input
+            column_name = f"{category}_{value}"
+            
+            # Check if the column exists in the feature columns
+            if column_name in X_dummies.columns:
+                user_input_df.loc[0, column_name] = 1
+            else:
+                # Handle the case where the selected value doesn't exist in the feature columns
+                print(({'error': f'Invalid selection: {column_name}'}))
+                # return jsonify({'error': f'Invalid selection: {column_name}'})
+
+        user_input_df = user_input_df.fillna(0)
+
+        print(user_input_df.head())
+
+        # Use your trained model to make a prediction
+        prediction = model.predict(user_input_df)
+
+        prediction_list = prediction.tolist()
+        print(f"prediction: {prediction}")
+        # Return the prediction as a response
+        return jsonify({'prediction': prediction_list})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,83 @@
-// Define variables for the scene, camera, and renderer
-// Create a loader for the OBJ model
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-
-// Set the size of the renderer
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Append the renderer to the container in your HTML
-document.getElementById('mushroom-container').appendChild(renderer.domElement);
-
-// Create a loader for the OBJ model
-const loader = new THREE.OBJLoader();
-
-// Load the OBJ model
-loader.load('/static/assets/mushroomlow.obj', (object) => {
-    // Add the loaded object to the scene
-    scene.add(object);
-
-    // Optionally, you can scale, position, or rotate the object as needed
-    object.scale.set(0.1, 0.1, 0.1);
-});
-
-// Set the initial camera position
-camera.position.z = 5;
-
-// Create an animation function
-const animate = () => {
-    // Add animation logic here if needed
-
-    // Render the scene with the camera
-    renderer.render(scene, camera);
-
-    // Request the next frame
-    requestAnimationFrame(animate);
+function init() {
+    d3.json("/api/v1/data/variables").then(function(data) {
+        create_data_entry(data);
+    });
 };
 
-// Start the animation loop
-animate();
+function create_data_entry(data) {
+    // Filter data for items of type "Categorical"
+    const categoricalData = data.filter(item => item.role === "Feature");
+  
+    // Select the container element (you may want to replace 'categories' with your actual container ID or selector)
+    const container = d3.select("#categories");
+  
+    // Create labels and dropdowns within the container
+    const dropdowns = container
+      .selectAll(".categorical-item")
+      .data(categoricalData)
+      .enter()
+      .append("div")
+      .attr("class", "categorical-item");
+  
+    dropdowns
+      .append("label")
+      .text(d => d.name.charAt(0).toUpperCase() + d.name.slice(1) + ": ");
+  
+    dropdowns
+      .append("select")
+      .selectAll("option")
+      .data(d => {
+        // Check if description is null or undefined before splitting
+        return d.description ? d.description.split(",") : [];
+      })
+      .enter()
+      .append("option")
+      .text(d => {
+        // Check if the item has the format key=value before displaying
+        const parts = d.split("=");
+        return parts.length === 2 ? parts[0] : d;
+      })
+      .attr("value", d => {
+        // Check if the item has the format key=value before setting value
+        const parts = d.split("=");
+        return parts.length === 2 ? parts[1] : d;
+      });
+  }
+  
+
+function is_edible() {
+    const selectedValues = {};
+  
+    d3.selectAll(".categorical-item select").each(function() {
+        const label = d3.select(this.parentNode).select("label").text().trim().replace(':', '').toLowerCase();
+ // Convert label to lowercase
+        const value = d3.select(this).property("value");
+        selectedValues[label] = value;
+    });
+
+    // Now 'selectedValues' is an object where the keys are the lowercase labels, and the values are the selected dropdown values.
+    // console.log(selectedValues);
+    
+    fetch('/api/v1/machine-learning/predict', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(selectedValues),
+      })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the API
+        console.log("Response JSON:", data);
+        const prediction = data.prediction; // Assuming your API returns a 'prediction' field
+        alert(`Prediction: ${prediction}`);
+        console.log(`Prediction: ${prediction}`);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Error occurred while predicting edibility.');
+      });
+}
+
+
+init();
